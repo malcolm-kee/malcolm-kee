7f02800ada4b65ac97f30879c3fc3fa4e06f2d8a
Malcolm Kee <malcolm.keeweesiong@gmail.com>
2018-04-29 20:55:25 +0800

    initial commit

---
+++ gatsby-config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  siteMetadata: {
+    title: 'Malcolm Kee',
+  },
+  plugins: ['gatsby-plugin-react-helmet'],
+}
---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Hi people</h1>
+    <p>Welcome to Malcolm Kee personal website.</p>
+    <p>This site is currently WIP.</p>
+    <Link to="/page-2/">Go to page 2</Link>
+  </div>
+)
+
+export default IndexPage
