38285bf6006e181f15d79d0080d205e6ec811297
Malcolm Kee <malcolm.keeweesiong@gmail.com>
2018-10-31 18:51:19 +0800

    add real quiz to project page

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -2,6 +2,7 @@ import { Link } from 'gatsby';
 import React from 'react';
 import Helmet from 'react-helmet';
 import krelosesMobileImg from '../assets/kreloses-mobile.gif';
+import realQuizApp from '../assets/real-quiz.gif';
 import simpleCartImg from '../assets/simple-cart.gif';
 import msiaEmploymentImg from '../assets/msia-employment.gif';
 import vueAppImg from '../assets/vue-movie-app.gif';
@@ -60,6 +61,46 @@ const ProjectPage = () => (
           <CardImage
             src={krelosesMobileImg}
             alt="Demo of Kreloses Mobile"
+            className="ProjectPage--demo-image"
+          />
+        </Card>
+        <Card className="ProjectPage--project">
+          <div>
+            <CardContent>
+              <h2>Real Quiz</h2>
+              <dl>
+                <dt>description</dt>
+                <dd>
+                  A polling application that allow instructors to give polling
+                  and get real-time polling results.
+                </dd>
+                <dt>technology/library</dt>
+                <dd>
+                  <ul>
+                    <li>Angular</li>
+                    <li>Angular Material</li>
+                    <li>Firebase (Firestore & Auth)</li>
+                    <li>RxJS</li>
+                    <li>Typescript</li>
+                  </ul>
+                </dd>
+              </dl>
+            </CardContent>
+            <CardActions>
+              <Button
+                component="a"
+                href="https://real-quiz-app.firebaseapp.com"
+                target="_BLANK"
+                color="primary"
+                raised
+              >
+                <Icon>launch</Icon> View
+              </Button>
+            </CardActions>
+          </div>
+          <CardImage
+            src={realQuizApp}
+            alt="Demo of Real Quiz"
             className="ProjectPage--demo-image"
           />
         </Card>
