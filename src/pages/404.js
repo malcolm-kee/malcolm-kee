ec6ff50fbd0d8d7be2a427c5b9abca49aae948f0
Malcolm Kee <malcolm.keeweesiong@gmail.com>
2019-04-01 04:23:13 +0800

    add new blog and tweak style

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,10 +1,18 @@
+import { Link } from 'gatsby';
 import React from 'react';
 
 const NotFoundPage = () => (
   <main className="main-content">
-    <div>
+    <div className="text-center">
       <h1>NOT FOUND</h1>
-      <p>The page doesn&#39;t exist... the sadness.</p>
+      <div>
+        <p>The page doesn&#39;t exist... the sadness.</p>
+        <nav className="Toolbar center">
+          <Link to="/" className="link-primary">
+            Home
+          </Link>
+        </nav>
+      </div>
     </div>
   </main>
 );
