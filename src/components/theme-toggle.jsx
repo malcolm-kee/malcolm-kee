c4fbba9bfc665c47ac21b21d1f6c3f4960c6878b
Malcolm Kee <malcolm.keeweesiong@gmail.com>
2019-06-06 16:34:59 +0800

    use icon for theme switcher

--- src/components/theme-toggle.jsx
+++ src/components/theme-toggle.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Toggle } from './toggle';
+import moon from '../assets/moon.png';
+import sun from '../assets/sun.png';
 import { useTheme } from '../theme';
 import './theme-toggle.scss';
 
@@ -7,8 +9,33 @@ export function ThemeToggle(toggleProps) {
   const { value, toggle } = useTheme();
   return (
     <div className="theme-toggle">
-      <span className="theme-toggle-label">Dark Mode</span>
-      <Toggle {...toggleProps} checked={value === 'dark'} onChange={toggle} />
+      <Toggle
+        {...toggleProps}
+        icons={{
+          checked: (
+            <img
+              src={moon}
+              width="16"
+              height="16"
+              role="presentation"
+              style={{ pointerEvents: 'none' }}
+              alt="Dark Mode"
+            />
+          ),
+          unchecked: (
+            <img
+              src={sun}
+              width="16"
+              height="16"
+              role="presentation"
+              style={{ pointerEvents: 'none' }}
+              alt="Light Mode"
+            />
+          )
+        }}
+        checked={value === 'dark'}
+        onChange={toggle}
+      />
     </div>
   );
 }
