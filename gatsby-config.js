d50629d6ff56fc576809cefc2b0705a8c21955a6
Malcolm Kee <malcolm.keeweesiong@gmail.com>
2019-06-12 16:38:14 +0800

    add key concept placeholder

--- gatsby-config.js
+++ gatsby-config.js
@@ -88,7 +88,8 @@ module.exports = {
           {
             resolve: `gatsby-remark-images`,
             options: {
-              maxWidth: 590,
+              maxWidth: 800,
+              // withWebp: true,
             },
           },
         ],
