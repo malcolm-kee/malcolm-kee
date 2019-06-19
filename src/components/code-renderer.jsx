852e89fa332b2bb84147679b3683246498bfc13b
Malcolm Kee <malcolm.keeweesiong@gmail.com>
2019-06-19 13:11:07 +0800

    use github theme for code editor in live mode

--- src/components/code-renderer.jsx
+++ src/components/code-renderer.jsx
@@ -1,5 +1,4 @@
 import Highlight, { defaultProps } from 'prism-react-renderer';
-import duotoneLight from 'prism-react-renderer/themes/duotoneLight';
 import nightOwl from 'prism-react-renderer/themes/nightOwl';
 import React from 'react';
 import { LiveEditor, LiveError, LivePreview, LiveProvider } from 'react-live';
@@ -22,7 +21,7 @@ export const CodeRenderer = ({
 
   const { value } = useTheme();
 
-  const theme = value === 'dark' ? nightOwl : duotoneLight;
+  const theme = value === 'dark' ? nightOwl : githubTheme;
 
   return language === 'js' || (live && language === 'jsx') ? (
     <CodeLiveEditor
@@ -208,3 +207,78 @@ function CopyCodeButton({ code }) {
     </Popover>
   );
 }
+
+const githubTheme = {
+  plain: {
+    color: '#393A34',
+    backgroundColor: '#f6f8fa',
+  },
+  styles: [
+    {
+      types: ['comment', 'prolog', 'doctype', 'cdata'],
+      style: {
+        color: '#999988',
+        fontStyle: 'italic',
+      },
+    },
+    {
+      types: ['namespace'],
+      style: {
+        opacity: 0.7,
+      },
+    },
+    {
+      types: ['string', 'attr-value'],
+      style: {
+        color: '#e3116c',
+      },
+    },
+    {
+      types: ['punctuation', 'operator'],
+      style: {
+        color: '#393A34',
+      },
+    },
+    {
+      types: [
+        'entity',
+        'url',
+        'symbol',
+        'number',
+        'boolean',
+        'variable',
+        'constant',
+        'property',
+        'regex',
+        'inserted',
+      ],
+      style: {
+        color: '#36acaa',
+      },
+    },
+    {
+      types: ['atrule', 'keyword', 'attr-name', 'selector'],
+      style: {
+        color: '#00a4db',
+      },
+    },
+    {
+      types: ['function', 'deleted', 'tag'],
+      style: {
+        color: '#d73a49',
+      },
+    },
+    {
+      types: ['function-variable'],
+      style: {
+        color: '#6f42c1',
+      },
+    },
+    {
+      types: ['tag', 'selector', 'keyword'],
+      style: {
+        color: '#00009f',
+      },
+    },
+  ],
+};
