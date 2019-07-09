23d7e422c8a3b0f5d1ebf980d06f63de0010e580
Malcolm Kee <malcolm.keeweesiong@gmail.com>
2019-07-09 23:29:39 +0800

    small change on project page

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -45,26 +45,92 @@ const ProjectCard = ({ project }) => (
   </Card>
 );
 
-const ProjectPage = ({ data: { allProjects } }) => (
-  <div className="ProjectPage">
-    <Seo title="Past Projects" />
-    <MainContent as="div">
-      <main>
-        <PageTitleContainer title="Past Projects" />
-        <div className="ProjectPage--project-container">
-          {allProjects.edges.map(({ node }) => (
-            <ProjectCard project={node} key={node.name} />
-          ))}
-        </div>
-      </main>
-      <nav className="Toolbar center">
-        <Link to="/" className="link-primary">
-          Home
-        </Link>
-      </nav>
-    </MainContent>
+const ProjectListView = ({ projects }) => (
+  <div className="ProjectPage--project-container">
+    {projects.map(({ node }) => (
+      <ProjectCard project={node} key={node.name} />
+    ))}
   </div>
 );
+
+const FancyProjectCard = ({ project }) => {
+  return (
+    <Card className="ProjectPage--project">
+      <div>
+        <CardContent>
+          <h2>{project.name}</h2>
+          <p>{project.description}</p>
+          <ul>
+            {project.technologies.map(tech => (
+              <li key={tech}>{tech}</li>
+            ))}
+          </ul>
+        </CardContent>
+        <CardActions className="ProjectPage--project-links">
+          {project.links.live && (
+            <Button
+              component={OutLink}
+              href={project.links.live}
+              color="primary"
+              raised
+            >
+              Live
+            </Button>
+          )}
+          {project.links.code && (
+            <Button component={OutLink} href={project.links.code} raised>
+              Code
+            </Button>
+          )}
+        </CardActions>
+      </div>
+      <CardImage
+        src={project.image.publicURL}
+        className="ProjectPage--demo-image"
+      />
+    </Card>
+  );
+};
+
+// TODO: install @reach/dialog and use that
+
+const FancyProjectView = ({ projects }) => (
+  // <div className="ProjectPage--project-grid-container">
+  <div>
+    {projects.map(({ node }) => (
+      <FancyProjectCard project={node} key={node.name} />
+    ))}
+  </div>
+);
+
+const ProjectPage = ({ data: { allProjects } }) => {
+  const [isRendered, setIsRendered] = React.useState(false);
+
+  React.useEffect(() => {
+    setIsRendered(true);
+  }, []);
+
+  return (
+    <div className="ProjectPage">
+      <Seo title="Past Projects" />
+      <MainContent as="div">
+        <main>
+          <PageTitleContainer title="Past Projects" />
+          {isRendered ? (
+            <FancyProjectView projects={allProjects.edges} />
+          ) : (
+            <ProjectListView projects={allProjects.edges} />
+          )}
+        </main>
+        <nav className="Toolbar center">
+          <Link to="/" className="link-primary">
+            Home
+          </Link>
+        </nav>
+      </MainContent>
+    </div>
+  );
+};
 
 export const query = graphql`
   query ProjectsPageQuery {
