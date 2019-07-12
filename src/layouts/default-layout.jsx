c33f39f22973a9344588e7d4cae2d8673e6f779c
Malcolm Kee <malcolm.keeweesiong@gmail.com>
2019-07-12 18:33:11 +0800

    add gatsby workshop content

--- src/layouts/default-layout.jsx
+++ src/layouts/default-layout.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Footer } from '../components/footer';
 import { Header } from '../components/Header';
-import { Seo } from '../components/Seo';
 
 export const Layout = ({ isRoot, children }) => (
   <div className="Layout">
