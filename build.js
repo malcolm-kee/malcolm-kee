60385ed62d8d659869f3ff099650766ccbb57e4f
Malcolm Kee <malcolm.keeweesiong@gmail.com>
2019-10-03 20:39:55 +0800

    update post build script

--- build.js
+++ build.js
@@ -3,23 +3,51 @@ const got = require('got');
 
 console.log(`Starting build.js`);
 
-const isLogExists = fs.existsSync('./netlify.log')
+const isLogExists = fs.existsSync('./netlify.log');
 
 if (isLogExists) {
-    console.log(`netlify.log exists. Trying to send a status to GitHub.`);
-    const rawLines = fs.readFileSync('./netlify.log').toString().split(/\n|\r\n/);
+  console.log(`netlify.log exists. Trying to send a status to GitHub.`);
+  const rawLines = fs
+    .readFileSync('./netlify.log')
+    .toString()
+    .split(/\n|\r\n/);
 
-    console.log(rawLines);
+  console.log(rawLines);
 
-    const lines = rawLines
-        .filter(line => /Live Draft URL/.test(line))
-        .map(line => line.match(/https:\/\/.*$/)[0]);
+  const lines = rawLines
+    .filter(line => /Live Draft URL/.test(line))
+    .map(line => line.match(/https:\/\/.*$/)[0]);
 
-    lines.forEach(previewUrl => {
-        console.log(`PreviewURL: ${previewUrl}`);
-        console.log(`PR slug: ${process.env.TRAVIS_REPO_SLUG}`);
-        console.log(`PR sha: ${process.env.TRAVIS_PULL_REQUEST_SHA}`);
+  Promise.all(
+    lines.map(previewUrl => {
+      console.log(`PreviewURL: ${previewUrl}`);
+      const repoSlug = process.env.TRAVIS_REPO_SLUG;
+      const prSha = prSha;
+      console.log(`PR slug: ${repoSlug}`);
+      console.log(`PR sha: ${prSha}`);
+      if (repoSlug && prSha) {
+        return got.post(
+          `https://api.github.com/repos/${repoSlug}/statuses/${prSha}`,
+          {
+            auth: `malcolm-kee:${process.env.GITHUB_TOKEN}`,
+            body: {
+              state: 'success',
+              target_url: previewUrl,
+              description: `Preview is ready`,
+              context: `ci/preview`,
+            },
+          }
+        );
+      } else {
+        return Promise.resolve();
+      }
     })
+  )
+    .then(result => console.log(result))
+    .catch(error => {
+      console.log(`Error when making request`);
+      console.log(error);
+    });
 } else {
-    console.log(`netlify.log does not exists.`)
+  console.log(`netlify.log does not exists.`);
 }
