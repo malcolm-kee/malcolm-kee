8459851ebdfaf3f34ce8495c5d213cce2b5b060e
Malcolm Kee <malcolm.keeweesiong@gmail.com>
2019-10-03 22:44:28 +0800

    update deploy script

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ set -e
 echo $TRAVIS_PULL_REQUEST;
 
 if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then 
-    yarn deploy;
+    yarn deploy --message "Production deploy from travis";
 else
     yarn deploy:preview --message "Deploy for commit $TRAVIS_PULL_REQUEST_SHA" > netlify.log;
 fi
