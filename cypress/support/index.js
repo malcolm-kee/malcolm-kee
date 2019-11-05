04dabd3c7311014071fdf80d36d7766c238bd0c1
Malcolm Kee <malcolm.keeweesiong@gmail.com>
2019-11-06 00:22:34 +0800

    disable ga for cypress test

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -14,7 +14,14 @@
 // ***********************************************************
 
 // Import commands.js using ES2015 syntax:
-import './commands'
+import './commands';
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
+
+Cypress.on('window:before:load', win => {
+  // because this is called before any scripts
+  // have loaded - the ga function is undefined
+  // so we need to create it.
+  win.ga = cy.stub().as('ga');
+});
