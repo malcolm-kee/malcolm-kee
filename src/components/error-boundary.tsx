94358fec5664886c3f4591ba85eff9b16ec1fe87
Malcolm Kee <malcolm.keeweesiong@gmail.com>
2020-01-24 21:51:10 +0800

    change error boundary to allow recover

--- src/components/error-boundary.tsx
+++ src/components/error-boundary.tsx
@@ -1,25 +1,42 @@
 import * as React from 'react';
+import { Button } from './Button';
 
-export class ErrorBoundary extends React.Component<{}, { hasError: boolean }> {
+export class ErrorBoundary extends React.Component<
+  {
+    children: React.ReactNode;
+  },
+  { hasError: boolean }
+> {
   state = {
     hasError: false,
   };
 
+  static getDerivedStateFromError() {
+    return { hasError: true };
+  }
+
   componentDidCatch(error: any, errorStack: any) {
-    this.setState(
-      {
-        hasError: true,
-      },
-      () => {
-        console.error(error);
-        console.error(errorStack);
-      }
-    );
+    console.error(error);
+    console.error(errorStack);
   }
 
   render() {
     return this.state.hasError ? (
-      <h1>Error happend!</h1>
+      <div className="max-w-sm mx-auto py-6 px-2 text-center">
+        <h1 className="text-3xl text-red-500">Error</h1>
+        <div className="py-4">
+          <Button
+            color="primary"
+            onClick={() =>
+              this.setState({
+                hasError: false,
+              })
+            }
+          >
+            Retry
+          </Button>
+        </div>
+      </div>
     ) : (
       <>{this.props.children}</>
     );
