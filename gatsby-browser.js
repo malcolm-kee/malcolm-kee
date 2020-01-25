3934d8c6c81ff28f9625b76153ef5481dd3e78fd
Malcolm Kee <malcolm.keeweesiong@gmail.com>
2020-01-26 06:15:58 +0800

    fix hash scroll position

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,5 +1,6 @@
 require('./src/styles/global.scss');
 require('./src/styles/animations.scss');
+const scrollTo = require('scroll-to-element');
 
 exports.onClientEntry = () => {
   // IntersectionObserver polyfill for gatsby-image (Safari, IE)
@@ -17,6 +18,25 @@ exports.onClientEntry = () => {
   }
 };
 
+function checkHash(location) {
+  const { hash } = location;
+  if (hash) {
+    const target = document.querySelector(hash);
+    if (target) {
+      scrollTo(target, {
+        offset: window.innerWidth >= 576 ? -70 : -10,
+        duration: 1,
+      });
+    }
+  }
+}
+
+exports.onInitialClientRender = () => {
+  window.addEventListener('hashchange', () => {
+    checkHash(window.location);
+  });
+};
+
 exports.onRouteUpdate = ({ location, prevLocation }) => {
   if (prevLocation !== null) {
     // client-side routing happened!
@@ -25,4 +45,6 @@ exports.onRouteUpdate = ({ location, prevLocation }) => {
       skipLink.focus();
     }
   }
+
+  checkHash(location);
 };
