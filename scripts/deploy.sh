589a0ce217434675bed864be9f9af192b59a581f
Malcolm Kee <malcolm.keeweesiong@gmail.com>
2020-03-01 18:27:11 +0800

    update deps

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,6 +3,17 @@ set -e
 
 echo $TRAVIS_PULL_REQUEST;
 
+HOME_HTML=./public/index.html
+
+if [[ -f "$HOME_HTML" ]]; then
+    echo "Verified build success";
+else
+    echo "Build fail, terminating...";
+    exit 1;
+fi
+
+echo "Deploying..."
+
 if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then 
     yarn deploy --message "Production deploy from travis";
 else
