8ab5d2801b20815840750bf4aab9161ca161ecd3
Malcolm Kee <malcolm.keeweesiong@gmail.com>
2020-11-10 23:12:15 +0800

    tweak design for recordings page

--- src/pages/recordings.js
+++ src/pages/recordings.js
@@ -4,11 +4,10 @@ import * as React from 'react';
 import { MainContent } from '../components/main-content';
 import { OutLink } from '../components/OutLink';
 import { Seo } from '../components/Seo';
-import styles from './recordings.module.scss';
 
 export default function Recordings({ data }) {
   return (
-    <>
+    <div className="px-4 md:px-8 py-3">
       <Seo title="Recordings by Malcolm Kee" />
       <MainContent>
         <div className="text-center my-6">
@@ -19,36 +18,45 @@ export default function Recordings({ data }) {
             Recordings of my webcasts/sharings
           </p>
         </div>
-        <ul className="max-w-4xl mx-auto px-2 sm:px-4">
+        <ul className="mt-12 grid gap-5 max-w-lg mx-auto lg:grid-cols-2 lg:max-w-none">
           {data.videos.nodes.map((video) => (
-            <li key={video.videoId} className="mb-8">
-              <div className="text-sm">
-                <time dateTime={video.publishedAt}>{video.date}</time>
-              </div>
+            <li
+              className="block rounded-lg shadow-lg overflow-hidden"
+              key={video.videoId}
+            >
               <OutLink
                 to={`https://youtube.com/watch?v=${video.videoId}`}
-                className="sm:float-right sm:pl-2"
+                className="block bg-white h-full group py-4"
               >
-                <Image
-                  fixed={video.localThumbnail.childImageSharp.fixed}
-                  className={`rounded overflow-hidden ${styles.img}`}
-                />
+                <div className="text-center mb-2 px-4">
+                  <h2 className="text-2xl font-semibold leading-7 group-hover:underline">
+                    {video.title}
+                  </h2>
+                  <div className="text-sm">
+                    <time dateTime={video.publishedAt}>{video.date}</time>
+                  </div>
+                </div>
+                <div className="lg:px-4">
+                  <div
+                    to={`https://youtube.com/watch?v=${video.videoId}`}
+                    className="text-center max-h-64 mb-3 overflow-hidden lg:float-right lg:w-1/2 lg:pl-2"
+                  >
+                    <Image
+                      fluid={video.localThumbnail.childImageSharp.fluid}
+                      className="rounded overflow-hidden -mt-10 lg:mt-0"
+                    />
+                  </div>
+                  <p className="px-4 lg:px-0 md:whitespace-pre-wrap">
+                    {video.description}
+                  </p>
+                  <div className="clearfix" />
+                </div>
               </OutLink>
-              <h2 className="text-2xl">
-                <OutLink
-                  to={`https://youtube.com/watch?v=${video.videoId}`}
-                  className="link"
-                >
-                  {video.title}
-                </OutLink>
-              </h2>
-              <p className="whitespace-pre-wrap">{video.description}</p>
-              <div className="clearfix" />
             </li>
           ))}
         </ul>
       </MainContent>
-    </>
+    </div>
   );
 }
 
@@ -63,8 +71,8 @@ export const query = graphql`
         description
         localThumbnail {
           childImageSharp {
-            fixed(width: 300) {
-              ...GatsbyImageSharpFixed_withWebp
+            fluid(maxWidth: 500) {
+              ...GatsbyImageSharpFluid_withWebp
             }
           }
         }
