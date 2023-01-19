2d95803fc0369ab80056170a03628051613d9c00
Malcolm Kee <malcolm.keeweesiong@gmail.com>
2023-01-20 00:40:47 +1100

    final edit of blog

--- apps/v2/src/components/Aside.tsx
+++ apps/v2/src/components/Aside.tsx
@@ -3,12 +3,13 @@ import * as React from 'react';
 
 export default function Aside(props: {
   children: React.ReactNode;
-  heading: string;
+  heading?: string;
 }) {
   return (
     <aside className="p-6 bg-sky-50">
-      <p className="text-xl text-sky-900 mt-0">{props.heading}</p>
-
+      {props.heading && (
+        <p className="text-xl text-sky-900 mt-0">{props.heading}</p>
+      )}
       <div className="prose">{props.children}</div>
     </aside>
   );
