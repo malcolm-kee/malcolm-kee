45649bb39790d10d36d73822987ccb5fa9ec4f5a
Malcolm Kee <malcolm.keeweesiong@gmail.com>
2023-01-26 01:01:54 +1100

    add theme switcher

--- apps/v2/src/components/workshop/workshop-header.tsx
+++ apps/v2/src/components/workshop/workshop-header.tsx
@@ -3,6 +3,7 @@ import * as React from 'react';
 import type { WorkshopMetadata } from '~/data/workshop-data';
 import type { WorkshopTocItem } from '~/data/workshop-helpers';
 import { HomeIcon } from '../icons';
+import { ThemeSelector } from '../theme-selector';
 import { WorkshopMobileNavigation } from './workshop-mobile-navigation';
 
 export const WorkshopHeader = ({
@@ -30,7 +31,7 @@ export const WorkshopHeader = ({
   return (
     <header
       className={clsx(
-        'sticky top-0 z-50 flex items-center sm:justify-between bg-white px-4 py-5 sm:py-3 shadow-md shadow-slate-900/5 transition duration-500 dark:shadow-none sm:px-6 lg:px-8',
+        'sticky top-0 z-50 flex items-center justify-between bg-white px-4 py-5 sm:py-3 shadow-md shadow-slate-900/5 transition duration-500 dark:shadow-none sm:px-6 lg:px-8',
         isScrolled
           ? 'dark:bg-slate-900/95 dark:backdrop-blur dark:[@supports(backdrop-filter:blur(0))]:bg-slate-900/75'
           : 'dark:bg-transparent'
@@ -43,7 +44,7 @@ export const WorkshopHeader = ({
           icon={<img src={workshop.iconUrl} className="h-9 w-auto" />}
         />
       </div>
-      <div className="flex shrink-0 sm:flex-grow sm:basis-0 items-center">
+      <div className="flex shrink-0 flex-grow sm:basis-0 items-center">
         <img src={workshop.iconUrl} className="h-9 w-auto" />
       </div>
       <div className="hidden sm:block relative px-3 overflow-hidden">
@@ -51,7 +52,8 @@ export const WorkshopHeader = ({
           {workshop.name}
         </span>
       </div>
-      <div className="hidden sm:flex basis-0 justify-end items-center gap-6 sm:gap-8 sm:flex-grow ">
+      <div className="flex basis-0 justify-end items-center gap-6 sm:gap-8 sm:flex-grow ">
+        <ThemeSelector className="relative z-10" />
         <a href="/" className="group" aria-label="Home">
           <HomeIcon className="w-6 h-6 text-slate-400 group-hover:text-slate-500 dark:group-hover:text-slate-300" />
         </a>
