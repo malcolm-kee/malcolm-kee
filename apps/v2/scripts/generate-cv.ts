121504c658a772b21eee25b5c6ede0cabaada36b
Malcolm Kee <malcolm.keeweesiong@gmail.com>
2023-02-18 02:24:53 +1100

    refactor: use separate service for generate pdf

--- apps/v2/scripts/generate-cv.ts
+++ apps/v2/scripts/generate-cv.ts
@@ -1,11 +1,27 @@
-import { chromium } from 'playwright';
+import fs from 'node:fs';
+import process from 'node:process';
+import { config } from 'dotenv';
+import { stream } from 'undici';
 import { fileName } from '../src/data/cv';
 
+config();
+
 (async function generateCv() {
-  const browser = await chromium.launch();
-  const ctx = await browser.newContext();
-  const page = await ctx.newPage();
-  await page.goto('http://127.0.0.1:8989/cv');
-  await page.pdf({ path: `dist/${fileName}` });
-  await browser.close();
+  const writeStream = fs.createWriteStream(`dist/${fileName}`);
+
+  await stream(
+    'https://page-to-pdf.fly.dev/screenshot',
+    {
+      method: 'POST',
+      body: JSON.stringify({
+        url: 'https://malcolmkee.com/cv',
+        key: process.env.API_KEY,
+      }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      opaque: writeStream,
+    },
+    ({ opaque }) => opaque as fs.WriteStream
+  );
 })();
