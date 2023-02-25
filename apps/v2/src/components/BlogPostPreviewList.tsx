b2c6c504c1f837f4b865457fa5cf7abec3f55186
Malcolm Kee <malcolm.keeweesiong@gmail.com>
2023-02-25 16:51:49 +1100

    add hero image animation

--- apps/v2/src/components/BlogPostPreviewList.tsx
+++ apps/v2/src/components/BlogPostPreviewList.tsx
@@ -9,7 +9,7 @@ export const BlogPostPreviewList = (
       <div
         {...props}
         className={clsx(
-          'flex flex-col gap-16 @md/bloglist:border-l-2 @md/bloglist:border-l-zinc-100',
+          'flex flex-col gap-8 @md/bloglist:border-l-2 @md/bloglist:border-l-zinc-100',
           props.className
         )}
       />
