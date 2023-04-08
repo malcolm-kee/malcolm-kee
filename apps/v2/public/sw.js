98580b7377a5054d834b9418d5f4baab5ce5593f
Malcolm Kee <malcolm.keeweesiong@gmail.com>
2023-04-08 13:08:50 +1000

    fix offline page caching

--- apps/v2/public/sw.js
+++ apps/v2/public/sw.js
@@ -6,14 +6,21 @@
  */
 function initWorker(worker) {
   const cacheName = 'offline-contents';
+  const offlinePagePath = '/offline/';
 
-  worker.addEventListener('install', (event) => {
+  const offlineDepsResponse = fetch('/_page-deps/offline.json').then((res) => res.json());
+
+  worker.addEventListener('install', (installEvent) => {
     async function cacheOfflinePage() {
       try {
-        const offlinePageResponse = await fetch('/_page-deps/offline.json');
-        const offlineDeps = await offlinePageResponse.json();
+        const offlineDeps = await offlineDepsResponse;
 
-        const files = ['/offline', ...offlineDeps.css, ...offlineDeps.images, ...offlineDeps.js];
+        const files = [
+          offlinePagePath,
+          ...offlineDeps.css,
+          ...offlineDeps.images,
+          ...offlineDeps.js,
+        ];
 
         return caches.open(cacheName).then((cache) => cache.addAll(files));
       } catch (err) {
@@ -21,40 +28,65 @@ function initWorker(worker) {
       }
     }
 
-    event.waitUntil(cacheOfflinePage());
+    installEvent.waitUntil(cacheOfflinePage());
   });
 
-  worker.addEventListener('fetch', (ev) => {
+  worker.addEventListener('activate', (activateEvent) => {
+    activateEvent.waitUntil(
+      caches.open(cacheName).then(async (cache) => {
+        const offlineDeps = await offlineDepsResponse;
+
+        const files = [
+          offlinePagePath,
+          ...offlineDeps.css,
+          ...offlineDeps.images,
+          ...offlineDeps.js,
+        ];
+
+        const allKeys = await cache.keys();
+        const staledKeys = allKeys.filter(
+          (requestInit) => !files.includes(new URL(requestInit.url).pathname)
+        );
+
+        await Promise.all(staledKeys.map((k) => cache.delete(k)));
+      })
+    );
+  });
+
+  worker.addEventListener('fetch', (fetchEvent) => {
     async function networkFirstAndFallbackOffline() {
       try {
-        const preloadResponse = await ev.preloadResponse;
+        const preloadResponse = await fetchEvent.preloadResponse;
         if (preloadResponse) {
           console.log('use preloadResponse', preloadResponse);
           return preloadResponse;
         }
 
-        const networkResponse = await fetch(ev.request);
+        const networkResponse = await fetch(fetchEvent.request);
 
         return networkResponse;
       } catch (error) {
         // this error happens only when offline, not
         // "error" status code such as 404 or 500
-        const cachedResponse = await caches.match(ev.request);
+        const cachedResponse = await caches.match(fetchEvent.request);
 
         if (cachedResponse) {
           console.log('use cachedResponse', cachedResponse);
           return cachedResponse;
         }
 
-        if (ev.request.mode === 'navigate') {
-          console.log('use offline html');
+        if (fetchEvent.request.mode === 'navigate') {
           // only returns offline page for page load request
-          return caches.open(cacheName).then((cache) => cache.match('/offline'));
+          console.log('use offline html');
+
+          const offlineCache = await caches.open(cacheName);
+
+          return await offlineCache.match(offlinePagePath);
         }
       }
     }
 
-    ev.respondWith(networkFirstAndFallbackOffline());
+    fetchEvent.respondWith(networkFirstAndFallbackOffline());
   });
 }
 
