91b5e4e6744fee1f505b8e638c2bd1e92134f90b
Malcolm Kee <malcolm.keeweesiong@gmail.com>
2023-04-08 13:24:46 +1000

    improve checkOnlineStatus impl

--- apps/v2/src/lib/check-online-status.ts
+++ apps/v2/src/lib/check-online-status.ts
@@ -1,37 +1,56 @@
 export interface CheckOnlineStatusOptions {
   onStatusChange: (isOnline: boolean) => void;
   initialOnline: boolean;
+  checkIntervalMs?: number;
+  timeoutMs?: number;
 }
 
-export const checkOnlineStatus = ({ onStatusChange, initialOnline }: CheckOnlineStatusOptions) => {
-  // I don't use navigator.onLine because it
+export const checkOnlineStatus = ({
+  onStatusChange,
+  initialOnline,
+  checkIntervalMs = 3000,
+  timeoutMs = 1000,
+}: CheckOnlineStatusOptions) => {
+  // I don't rely navigator.onLine because it
   // sometimes does not detect extremely slow network,
   // so the solution here is to fetch a simple noop response
   // from server
   let lastStatus = initialOnline;
+  let timerId: ReturnType<typeof setTimeout>;
 
-  const intervalId = setInterval(() => {
-    const randomId = Date.now();
-
+  function getOnlineStatus() {
     Promise.race([
-      fetch(`/online.json?${randomId}`)
+      fetch(`/online.json?${Date.now()}`) // add a random id to avoid cached response
         .then(() => {
           return true;
         })
         .catch(() => {
           return false;
         }),
-      new Promise<boolean>((fulfill) => setTimeout(() => fulfill(false), 1000)),
-      // considered as offline when no response after 1sec
+      new Promise<boolean>((fulfill) => setTimeout(() => fulfill(false), timeoutMs)),
+      // considered as offline when no response after timeout
     ]).then((isOnline) => {
       if (lastStatus !== isOnline) {
         onStatusChange(isOnline);
         lastStatus = isOnline;
       }
+      timerId = setTimeout(getOnlineStatus, checkIntervalMs);
     });
-  }, 3000);
+  }
+
+  timerId = setTimeout(getOnlineStatus, checkIntervalMs);
+
+  function onOnlineEvent() {
+    clearTimeout(timerId);
+    getOnlineStatus();
+  }
+
+  window.addEventListener('online', onOnlineEvent);
+  window.addEventListener('offline', onOnlineEvent);
 
   return function cleanup() {
-    clearInterval(intervalId);
+    clearTimeout(timerId);
+    window.removeEventListener('online', onOnlineEvent);
+    window.removeEventListener('offline', onOnlineEvent);
   };
 };
