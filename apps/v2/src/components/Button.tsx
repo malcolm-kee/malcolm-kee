9fe28fe9a3a4250b8e407b35db1259aaa5f1f4a8
Malcolm Kee <malcolm.keeweesiong@gmail.com>
2023-05-14 17:55:08 +1000

    style: tweak button design

--- apps/v2/src/components/Button.tsx
+++ apps/v2/src/components/Button.tsx
@@ -1,27 +1,40 @@
 import { clsx } from 'clsx';
 import * as React from 'react';
 
-const variantStyles = {
+export const variantStyles = {
   primary:
-    'bg-zinc-800 font-semibold text-zinc-100 hover:bg-zinc-700 active:bg-zinc-800 active:text-zinc-100/70 disabled:bg-zinc-500',
+    'bg-primary-800 font-semibold text-white border-transparent hover:bg-primary-900/90 disabled:bg-[#6cbeb8]',
   secondary:
-    'bg-zinc-50 font-medium text-zinc-900 hover:bg-zinc-100 active:bg-zinc-100 active:text-zinc-900/60',
+    'bg-white font-medium text-primary-800 border-transparent hover:bg-zinc-50/90 active:bg-zinc-50 active:text-primary-900 disabled:bg-white disabled:text-[#83aeac]',
 };
+
+export const raisedStyles = {
+  primary:
+    'border-b-primary-900 border-l-primary-900/20 border-r-primary-900/10 active:border-transparent',
+  secondary: 'border-b-zinc-300 active:border-transparent',
+};
+
+export interface ButtonProps extends React.ComponentPropsWithoutRef<'button'> {
+  variant?: 'primary' | 'secondary';
+  flat?: boolean;
+  href?: string;
+  download?: boolean;
+}
 
 export function Button({
   variant = 'primary',
+  disabled,
+  flat = disabled,
   href,
   download,
   ...props
-}: React.ComponentPropsWithoutRef<'button'> & {
-  variant?: 'primary' | 'secondary';
-  href?: string;
-  download?: boolean;
-}) {
+}: ButtonProps) {
   const className = clsx(
-    'inline-flex items-center gap-2 justify-center rounded-md py-2 px-3 text-sm outline-offset-2 transition active:transition-none',
+    'inline-flex items-center gap-2 justify-center rounded-md py-2 px-3 text-sm outline-offset-2 transition active:transition-none border',
     variantStyles[variant],
     'data-[success]:bg-emerald-600 data-[success]:text-zinc-100 data-[success]:hover:bg-emerald-500 data-[success]:active:bg-emerald-600',
+    !disabled && !flat && 'active:translate-y-px',
+    !flat && ['shadow active:shadow-none', raisedStyles[variant]],
     props.className
   );
 
@@ -33,5 +46,5 @@ export function Button({
     );
   }
 
-  return <button type="button" {...props} className={className} />;
+  return <button type="button" {...props} disabled={disabled} className={className} />;
 }
