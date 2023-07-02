cc127db88b20cf0da261e4a8a7aca954c1e6e3c3
Malcolm Kee <malcolm.keeweesiong@gmail.com>
2023-07-02 14:47:53 +1000

    minor tweak

--- apps/v2/src/components/icons.tsx
+++ apps/v2/src/components/icons.tsx
@@ -215,13 +215,8 @@ export function MenuIcon(props: IconProps) {
 
 export function ArrowLeftIcon(props: IconProps) {
   return (
-    <svg viewBox="0 0 16 16" fill="none" aria-hidden="true" {...props}>
-      <path
-        d="M7.25 11.25 3.75 8m0 0 3.5-3.25M3.75 8h8.5"
-        strokeWidth="1.5"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      />
+    <svg viewBox="0 0 24 24" fill="none" aria-hidden="true" strokeWidth={1.5} {...props}>
+      <path strokeLinecap="square" strokeLinejoin="miter" d="M10.5 19.5L3 12m0 0l7.5-7.5M3 12h18" />
     </svg>
   );
 }
