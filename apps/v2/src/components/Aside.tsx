7962a8c472d22ad422b3e610f32a6120a869bfdc
Malcolm Kee <malcolm.keeweesiong@gmail.com>
2023-07-08 23:20:52 +1000

    design tweak: include layout tweak for 2xl

--- apps/v2/src/components/Aside.tsx
+++ apps/v2/src/components/Aside.tsx
@@ -1,23 +1,37 @@
 import * as React from 'react';
+import { clsx } from 'clsx';
 
 export default function Aside(props: {
   children: React.ReactNode;
   heading?: string;
   className?: string;
+  static?: boolean;
 }) {
   return (
     <div data-component="aside" className={props.className}>
-      <aside>
-        <div className="relative p-6">
-          <div className="absolute inset-0 bg-sky-50 shadow" />
+      <div className="relative">
+        <aside
+          className={clsx(
+            'bg-sky-50 shadow max-w-2xl mx-auto p-6',
+            !props.static &&
+              '2xl:absolute 2xl:left-full 2xl:w-80 2xl:bottom-0 2xl:translate-y-1/2 2xl:bg-transparent 2xl:shadow-none'
+          )}
+        >
           <div className="relative">
             {props.heading && (
-              <p className="text-xl text-sky-900 mt-0">{props.heading}</p>
+              <p
+                className={clsx(
+                  'text-sky-900 mt-0',
+                  props.static ? 'text-xl' : 'text-xl 2xl:text-base'
+                )}
+              >
+                {props.heading}
+              </p>
             )}
-            <div className="prose">{props.children}</div>
+            <div className="prose prose-sm">{props.children}</div>
           </div>
-        </div>
-      </aside>
+        </aside>
+      </div>
     </div>
   );
 }
