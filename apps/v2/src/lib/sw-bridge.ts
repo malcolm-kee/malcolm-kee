8229b64816a0a6f657328c5916012de14806e474
Malcolm Kee <malcolm.keeweesiong@gmail.com>
2023-07-15 00:18:07 +1000

    feat: add push notification

--- apps/v2/src/lib/sw-bridge.ts
+++ apps/v2/src/lib/sw-bridge.ts
@@ -1,5 +1,17 @@
+declare global {
+  interface Window {
+    __swRegistration: ServiceWorkerRegistration;
+  }
+}
+
 export const register = () => {
   if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/sw.js'); // in public folder
+    navigator.serviceWorker
+      .register('/sw.js') // in public folder
+      .then((registration) => {
+        window.__swRegistration = registration;
+      });
   }
 };
+
+export const getServiceWorkerRegistration = () => window.__swRegistration;
