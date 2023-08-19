7b0ec30a538fa677c9f81550833badc2e00f6d33
Malcolm Kee <malcolm.keeweesiong@gmail.com>
2023-08-19 13:14:04 +1000

    lox interpret expression

---
+++ apps/v2/src/content/note/_crafting_interpreters/v1/com/craftinginterpreters/lox/Interpreter.java
@@ -0,0 +1,157 @@
+package com.craftinginterpreters.lox;
+
+import com.craftinginterpreters.lox.Expr.Binary;
+import com.craftinginterpreters.lox.Expr.Grouping;
+import com.craftinginterpreters.lox.Expr.Unary;
+
+public class Interpreter implements Expr.Visitor<Object> {
+
+    void interpret(Expr expression) {
+        try {
+            Object value = evaluate(expression);
+            System.out.println(stringify(value));
+        } catch (RuntimeError error) {
+            Lox.runtimeError(error);
+        }
+    }
+
+    private String stringify(Object object) {
+        if (object == null) return "nil";
+
+        if (object instanceof Double) {
+            String text = object.toString();
+            if (text.endsWith(".0")) {
+                text = text.substring(0, text.length() - 2);
+            }
+            return text;
+        }
+
+        return object.toString();
+    }
+
+    @Override
+    public Object visitLiteralExpr(Expr.Literal expr) {
+        return expr.value;
+    }
+
+    @Override
+    public Object visitBinaryExpr(Binary expr) {
+        var left = evaluate(expr.left);
+        var right = evaluate(expr.right);
+
+        switch (expr.operator.type) {
+            case GREATER:
+                checkNumberOperands(expr.operator, left, right);
+                return (double) left > (double) right;
+
+            case GREATER_EQUAL:
+                checkNumberOperands(expr.operator, left, right);
+                return (double) left >= (double) right;
+
+            case LESS:
+                checkNumberOperands(expr.operator, left, right);
+                return (double) left < (double) right;
+
+            case LESS_EQUAL:
+                checkNumberOperands(expr.operator, left, right);
+                return (double) left <= (double) right;
+
+            case EQUAL_EQUAL:
+                return isEqual(left, right);
+
+            case BANG_EQUAL:
+                return !isEqual(left, right);
+
+            case MINUS:
+                checkNumberOperands(expr.operator, left, right);
+                return (double) left - (double) right;
+
+            case SLASH:
+                checkNumberOperands(expr.operator, left, right);
+                return (double) left / (double) right;
+
+            case STAR:
+                checkNumberOperands(expr.operator, left, right);
+                return (double) left * (double) right;
+
+            case PLUS:
+                if (left instanceof Double && right instanceof Double) {
+                    return (double) left + (double) right;
+                }
+
+                if (left instanceof String && right instanceof String) {
+                    return (String) left + (String) right;
+                }
+
+                if (left instanceof String && right instanceof Double) {
+                    return (String) left + ((double) right + "");
+                }
+
+                if (left instanceof Double && right instanceof String) {
+                    return ((double) left + "") + (String) right;
+                }
+
+                throw new RuntimeError(expr.operator, "Operands must be two numbers or two strings.");
+        
+            default:
+                break;
+        }
+
+
+        return null;
+    }
+
+    @Override
+    public Object visitGroupingExpr(Grouping expr) {
+        return this.evaluate(expr);
+    }
+
+    @Override
+    public Object visitUnaryExpr(Unary expr) {
+        var right = evaluate(expr.right);
+
+        switch (expr.operator.type) {
+            case BANG:
+                return !isTruthy(right);
+
+            case MINUS:
+                checkNumberOperand(expr.operator, right);
+                return -(double)right;
+        
+            default:
+                break;
+        }
+
+        // not reachable
+        return null;
+    }
+
+    private void checkNumberOperand(Token operator, Object operand) {
+        if (operand instanceof Double) return;
+        throw new RuntimeError(operator, "Operand must be a number.");
+    }
+
+    private void checkNumberOperands(Token operator, Object a, Object b) {
+        if (a instanceof Double && b instanceof Double) return;
+
+        throw new RuntimeError(operator, "Operands must be numbers");
+    }
+
+    private Object evaluate(Expr expr) {
+        return expr.accept(this);
+    }
+
+    private boolean isTruthy(Object object) {
+        if (object == null) return false;
+        if (object instanceof Boolean) return (boolean) object;
+        return true;
+    }
+
+    private boolean isEqual(Object a, Object b) {
+        if (a == null && b == null) return true;
+
+        if (a == null) return false;
+
+        return a.equals(b);
+    }
+}
--- apps/v2/src/content/note/_crafting_interpreters/v1/com/craftinginterpreters/lox/Lox.java
+++ apps/v2/src/content/note/_crafting_interpreters/v1/com/craftinginterpreters/lox/Lox.java
@@ -9,7 +9,10 @@ import java.nio.file.Paths;
 import java.util.List;
 
 public class Lox {
+    private static final Interpreter interpreter = new Interpreter();
+
     static boolean hadError = false;
+    static boolean hadRuntimeError = false;
 
     public static void main(String[] args) throws IOException {
         if (args.length > 1) {
@@ -27,6 +30,7 @@ public class Lox {
         run(new String(bytes, Charset.defaultCharset()));
 
         if (hadError) System.exit(65);
+        if (hadRuntimeError) System.exit(70);
     }
 
     private static void runPrompt() throws IOException {
@@ -52,7 +56,7 @@ public class Lox {
         // Stop if there is syntax error
         if (hadError) return;
 
-        System.out.println(new AstPrinter().print(expression));
+        interpreter.interpret(expression);
     }
 
     static void error(int line, String message) {
@@ -65,6 +69,11 @@ public class Lox {
         } else {
             report(token.line, "at '" + token.lexeme + "'", message);
         }
+    }
+
+    static void runtimeError(RuntimeError error) {
+        System.err.println(error.getMessage() + "\n[line" + error.token.line + "]");
+        hadRuntimeError = true;
     }
 
     private static void report(int line, String where, String message) {
